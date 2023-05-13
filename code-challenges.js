22855051e4ebb3b927a486282f5e64663c0737bd
Chris-Frey <chris.frey@live.com>
2023-05-13 09:58:19 -0700

    code challenge complete

--- code-challenges.js
+++ code-challenges.js
@@ -31,8 +31,8 @@ const fruitSize = (fruitA, fruitB) => {
     return "They are the same size"
   }
 }
-console.log(fruitSize(fruit1, fruit2));
-console.log(fruitSize(fruit3, fruit4));
+console.log(fruitSize(fruit1, fruit2)); // banana
+console.log(fruitSize(fruit3, fruit4)); // cherry
 
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
@@ -53,32 +53,48 @@ const temperature3 = 212
 let boiling = 212
 const boilingPoint = (temp) => {
   if(temp === boiling) {
-    return `${temp} is at boiling point`
+    return `${temp} is at boiling point`;
   }
-  return (temp > boiling ? `${temp} is above boiling point` : `${temp} is below boiling point`)
+  return (temp > boiling ? `${temp} is above boiling point` : `${temp} is below boiling point`);
 }
-console.log(boilingPoint(temperature1));
-console.log(boilingPoint(temperature2));
-console.log(boilingPoint(temperature3));
+console.log(boilingPoint(temperature1)); // 42 is below boiling point
+console.log(boilingPoint(temperature2)); // 350 is above boiling point
+console.log(boilingPoint(temperature3)); // 212 is at boiling point
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Pseudo code:
+// combine the arrays with concat
+// use .length to get the answer
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
+const runs = (arr1, arr2) => {
+  return (arr1.concat(arr2)).length;
+}
+console.log(runs(padres1984WorldSeriesRuns, padres1998WorldSeriesRuns)); // 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Pseudo code:
+// create arrow function that takes 1 argument
+// split the string into an array
+// use reverse command on array 
+// join the reversed array
 
 const currentCohort = "Delta 2023"
 // Expected output: "3202 atleD"
+const reverseMe = (arg) => {
+  return arg.split('').reverse().join('');
+}
+console.log(reverseMe(currentCohort)); // 3202 atleD
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Pseudo code:
+// create arrow function with 1 parameter
+// use lastIndexOf to find the last index of the given argument and return that value
 
 const numberOfConnections = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 
@@ -87,13 +103,26 @@ const givenValue1 = 42
 
 const givenValue2 = 10
 // Expected output: 8
+const lastNumIndex = (num) => {
+  return numberOfConnections.lastIndexOf(num);
+}
+console.log(lastNumIndex(givenValue1)); // 7
+console.log(lastNumIndex(givenValue2)); // 8
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
+// create arrow function that takes an array argument
+// call sort on the array and then reverse
+// return that output
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+const sortedTemps = (arr) => {
+  return arr.sort().reverse();
+}
+console.log(sortedTemps(sanDiegoSummerTemperatures)); // [82, 80, 79, 77, 76, 73, 72]
+console.log(sortedTemps(sanDiegoWinterTemperatures)); // [68, 67, 66, 66, 62, 59, 59]
